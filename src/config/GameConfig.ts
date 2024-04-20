194c628bc45e898ad2f9185f4d8aeb1314d57064
Dakota Madden-Fong <maddenfong@gmail.com>
2024-04-20 09:53:22 -0700

    work

--- src/config/GameConfig.ts
+++ src/config/GameConfig.ts
@@ -1,6 +1,5 @@
 const GameConfig = {
-    width: 1000,
-    height: 1000,
+    size: 1000,
 };
 
 export default GameConfig;
--- src/game/component/entity/Rock.ts
+++ src/game/component/entity/Rock.ts
@@ -1,17 +1,30 @@
+import { Container, Graphics, GraphicsContext } from "pixi.js";
+
+import { Vector } from "../../model/Vector";
+import { Entity } from "./Entity";
 
 class Rock implements Entity {
     location: Vector;
-    size: number;
-    private graphics: GraphicsContext;
-    private static readonly graphics
-    constructor(location: Vector, size: number) {
 
+    size: number;
+
+    private graphics: Graphics;
+
+    private static readonly graphicsContext: GraphicsContext =
+        new GraphicsContext()
+            .circle(0, 0, 1)
+            .fill({ color: 0x888888 })
+            .stroke({ width: 2, color: 0xbbbbbb });
+
+    constructor(location: Vector, size: number) {
         this.location = location;
         this.size = size;
-        this.graphics = new Graphics();
+        this.graphics = new Graphics(Rock.graphicsContext);
     }
+
     getContainer(): Container {
-        throw new Error("Method not implemented.");
+        return this.graphics;
     }
+
     update(screenSize: number, screenPosition: Vector) {}
 }
